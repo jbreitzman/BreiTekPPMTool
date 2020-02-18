61c2c9868b87a7cea543b7b99a7d865bf7f3d64b
jbreitzman <jbreitzman@gmail.com>
2020-02-18 17:53:22 -0500

    Added custom exception handler

--- PPMTool/src/main/java/io/breitek/PPMTool/services/ProjectService.java
+++ PPMTool/src/main/java/io/breitek/PPMTool/services/ProjectService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import io.breitek.PPMTool.domain.Project;
+import io.breitek.PPMTool.exceptions.ProjectIdException;
 import io.breitek.PPMTool.repositories.ProjectRepository;
 
 @Service
@@ -13,9 +14,22 @@ public class ProjectService {
 	private ProjectRepository projectRepository;
 	
 	public Project saveOrUpdateProject(Project project) {
+		try {
+			project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+			return projectRepository.save(project);
+		} catch (Exception e) {
+			throw new ProjectIdException("Project Id '" + project.getProjectIdentifier().toUpperCase() + "' already exists.");
+		}
+	}
+	
+	public Project findProjectByIdentifier(String projectId) {
 		
-		// Logic
+		Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
 		
-		return projectRepository.save(project);
+		if(project == null) {
+			throw new ProjectIdException("Project Id '" + projectId + "' does not exist.");
+		}
+		 
+		return project;
 	}
 }
